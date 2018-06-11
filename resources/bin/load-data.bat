7eb03813a3c44d4d2d17cb4c756dd4b86650dc38
Stéphane Dumont <dumont.stephane@gmail.com>
2018-06-12 01:41:17 +0200

    Bin

--- resources/bin/load-data.bat
+++ resources/bin/load-data.bat
@@ -1,6 +1,6 @@
-D:\Tools\Xampp\mysql\bin\mysql.exe -uroot -p -Delevage <D:\Projets\Sources\elevage\resources\sql\02-pays.sql
-D:\Tools\Xampp\mysql\bin\mysql.exe -uroot -p -Delevage <D:\Projets\Sources\elevage\resources\sql\03-localites-be.sql
-D:\Tools\Xampp\mysql\bin\mysql.exe -uroot -p -Delevage <D:\Projets\Sources\elevage\resources\sql\04-localites-ch.sql
-D:\Tools\Xampp\mysql\bin\mysql.exe -uroot -p -Delevage <D:\Projets\Sources\elevage\resources\sql\05-localites-fr.sql
-D:\Tools\Xampp\mysql\bin\mysql.exe -uroot -p -Delevage <D:\Projets\Sources\elevage\resources\sql\06-localites-lu.sql
-D:\Tools\Xampp\mysql\bin\mysql.exe -uroot -p -Delevage <D:\Projets\Sources\elevage\resources\sql\07-localites-de.sql
+D:\Tools\Xampp\mysql\bin\mysql.exe -u root --password="" elevage <D:\Projets\Sources\elevage\resources\sql\02-pays.sql
+D:\Tools\Xampp\mysql\bin\mysql.exe -u root --password="" elevage <D:\Projets\Sources\elevage\resources\sql\03-localites-be.sql
+D:\Tools\Xampp\mysql\bin\mysql.exe -u root --password="" elevage <D:\Projets\Sources\elevage\resources\sql\04-localites-ch.sql
+D:\Tools\Xampp\mysql\bin\mysql.exe -u root --password="" elevage <D:\Projets\Sources\elevage\resources\sql\05-localites-fr.sql
+D:\Tools\Xampp\mysql\bin\mysql.exe -u root --password="" elevage <D:\Projets\Sources\elevage\resources\sql\06-localites-lu.sql
+D:\Tools\Xampp\mysql\bin\mysql.exe -u root --password="" elevage <D:\Projets\Sources\elevage\resources\sql\07-localites-de.sql
