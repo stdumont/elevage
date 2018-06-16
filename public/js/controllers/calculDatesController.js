9df761bdc2f043bc9b9d8d2f8fb385b6fd1f8be6
Stéphane Dumont <dumont.stephane@gmail.com>
2018-06-16 04:11:29 +0200

    Calcul dates

--- public/js/controllers/calculDatesController.js
+++ public/js/controllers/calculDatesController.js
@@ -3,12 +3,12 @@ angular.module('elevageApp').controller('calculDatesController', ['$scope', '$ro
     // Mettre à jour le menu de navigation avec le lien courant.
     refreshCurrentLink($route.current.activeTab);
 
-    $scope.calcul_1_date_depart = moment().format('DD/MM/YYYY');
-    $scope.calcul_1_nombre = 57;
+    $scope.calcul_1_date_depart = null;
+    $scope.calcul_1_nombre = null;
     $scope.calcul_1_date_resultat = null;
 
-    $scope.calcul_2_date_debut = moment().format('DD/MM/YYYY');
-    $scope.calcul_2_date_fin = moment().format('DD/MM/YYYY');
+    $scope.calcul_2_date_debut = null;
+    $scope.calcul_2_date_fin = null;
     $scope.calcul_2_nombre_resultat = null;
 
     $scope.calcul1 = function() {
