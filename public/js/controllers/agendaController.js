75ba52b274e0aecb73ee2611dbe9dc0014242167
Stéphane Dumont <dumont.stephane@gmail.com>
2018-06-22 03:35:22 +0200

    Agenda

--- public/js/controllers/agendaController.js
+++ public/js/controllers/agendaController.js
@@ -8,73 +8,79 @@ angular.module('elevageApp').controller('agendaController', ['$scope', '$route',
     $scope.calendarStart = null;
     $scope.calendarEnd = null;
     $scope.dataEvent = null;
+    $scope.modalTitle = null;
+    $scope.modalTitleAdd = "Ajouter un événement";
+    $scope.modalTitleUpdate = 'Modifier un événement';
 
     //
     // Gestion du click sur un événement
     //
     $scope.onEventClick = function(event) {
-        console.log('eventClick');
-        console.log(event);
+        // console.log('eventClick');
+        // console.log(event);
         $scope.calendarEvent = event;
         $scope.calendarStart = event.start;
         $scope.calendarEnd = event.end;
-        console.log($scope.calendarEvent);
-        console.log($scope.calendarStart);
-        console.log($scope.calendarEnd);
+        $scope.modalTitle = $scope.modalTitleUpdate;
+        // console.log($scope.calendarEvent);
+        // console.log($scope.calendarStart);
+        // console.log($scope.calendarEnd);
         // $('#modalTitle').html(event.title);
         // $('#modalBody').html(event.description);
-        // $('#fullCalModal').modal();
+        $('#fullCalendarModal').modal();
     };
 
     //
     // Gestion du click sur une journée/heure
     //
     $scope.onSelect = function(start, end) {
-        console.log('select');
-        console.log(start);
-        console.log(end);
+        $scope.modalTitle = "Ajouter un événement";
+        // console.log('select');
+        // console.log(start);
+        // console.log(end);
         $scope.calendarEvent = null;
         $scope.calendarStart = start;
         $scope.calendarEnd = end;
-        console.log($scope.calendarEvent);
-        console.log($scope.calendarStart);
-        console.log($scope.calendarEnd);
+        // console.log($scope.calendarEvent);
+        // console.log($scope.calendarStart);
+        // console.log($scope.calendarEnd);
+        $('#fullCalendarModal').modal();
     };
 
     // MAIN
-    $(document).ready(function() {
+    // $(document).ready(function() {
 
-        $('#agenda').fullCalendar({
-            locale: 'fr',
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,basicWeek,basicDay,listMonth'
-            },
-            navLinks: true,
-            selectable: true,
-            eventLimit: true, // permet d'avoir '+plus' si trop d'événement sur une journée
-            // clic sur un événement
-            eventClick: function(calEvent, jsEvent, view) {
-                $scope.onEventClick(calEvent);
-            },
-            // modification des données avant le rendu
-            eventRender: function(event, element) {
-                if (event.description) {
-                    element.qtip({
-                        content: event.description
-                    });
-                }
-            },
-            // clic sur une journée; typiquement pour ajouter un événement
-            select: function(start, end, event, view, resource) {
-                $scope.onSelect(start, end);
-            },
-            // source des événement affichés par accès à l'api
-            events: '/api/agenda/retrieve',
-
-        });
+    $('#agenda').fullCalendar({
+        locale: 'fr',
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay,listMonth'
+        },
+        navLinks: true,
+        selectable: true,
+        eventLimit: true, // permet d'avoir '+plus' si trop d'événement sur une journée
+        // clic sur un événement
+        eventClick: function(calEvent, jsEvent, view) {
+            $scope.onEventClick(calEvent);
+        },
+        // modification des données avant le rendu
+        eventRender: function(event, element) {
+            if (event.description) {
+                element.qtip({
+                    content: event.description
+                });
+            }
+        },
+        // clic sur une journée; typiquement pour ajouter un événement
+        select: function(start, end, event, view, resource) {
+            $scope.onSelect(start, end);
+        },
+        // source des événement affichés par accès à l'api
+        events: '/api/agenda/retrieve',
 
     });
+
+    // });
 
 }]);
